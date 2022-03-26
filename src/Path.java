6efc14fcba215d39fd5bb58908015476e89e5510
Aadila Ali Sabry <aalisabr@uwaterloo.ca>
2022-03-26 13:27:04 -0400

    refactored Path so it doesn't inherit from ArrayList<Step>

--- src/Path.java
+++ src/Path.java
@@ -1,20 +1,39 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class Path extends ArrayList <Step> {
-    //returns the length of a path
+public class Path implements Iterable <Step> {
+    private ArrayList <Step> path;
+
+    public Path () {
+        path = new ArrayList <Step> ();
+    }
+
+    @Override
+    public Iterator <Step> iterator() {
+        return path.iterator();
+    }
+
     public int length () {
         int length = 0;
         for (Step step : this) length += step.getSteps(); //Step step : this (is this valid?)
         return length;
     }
 
+    public int size () {
+        return path.size ();
+    }
+
     private Step getLast () {
-        return get (size () - 1);
+        return path.get (size () - 1);
     }
 
     private void addStepToOriginal (Step.Direction direction) {
-        if (size () != 0 && getLast ().getDirection () == direction) getLast ().incrementSteps ();
-        else add (new Step (direction, 1));
+        addStepToOriginal (new Step (direction, 1));
+    }
+
+    public void addStepToOriginal (Step step) {
+        if (size () != 0 && step.getDirection () == getLast ().getDirection ()) path.set (path.size () - 1, new Step (step.getDirection (), getLast ().getSteps () + step.getSteps ()));
+        else path.add (step);
     }
 
     //adds to a copy and returns copy
@@ -26,7 +45,7 @@ public class Path extends ArrayList <Step> {
 
     private Path deepCopy () {
         Path newPath = new Path ();
-        for (Step step : this) newPath.add (step.copyStep ());
+        for (Step step : this) newPath.addStepToOriginal (step.copyStep ());
         return newPath;
     }
 }
